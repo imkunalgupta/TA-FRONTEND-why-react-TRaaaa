cce53494723e02290401cbce8c7009ab8a0476e6
Kunal <mailto.guptakunal@gmail.com>
2023-01-12 18:29:43 +0530

    updated news app with prop-types

---
+++ block-BRaaaq/news-app/src/Components/Footer.js
@@ -0,0 +1,23 @@
+function Footer() {
+  return (
+    <>
+      <div className="container">
+        <div className="flex footer">
+          <small>2023 &copy; All Rights Reserved</small>
+          <ul className="flex">
+            <li>
+              <i class="fa fa-facebook" aria-hidden="true"></i>
+            </li>
+            <li>
+              <i class="fa fa-twitter"></i>
+            </li>
+            <li>
+              <i class="fa fa-instagram"></i>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Footer;
---
+++ block-BRaaaq/news-app/src/Components/Hero.js
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <>
+      <div className="hero">
+        <img
+          src="/images/hero.jpeg"
+          alt=""
+          width="100%"
+          height="350px"
+          background-size="cover"
+        />
+      </div>
+    </>
+  );
+}
+
+export default Hero;
